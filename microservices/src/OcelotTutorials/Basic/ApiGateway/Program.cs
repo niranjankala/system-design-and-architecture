4d59fb3a4c9da29648f471fefdb1eed8f599e69b
Niranjan Singh <nsingh@stpis.com>
2024-03-27 21:41:35 +0530

    Code to configure the ocelot to use AD authentication

--- microservices/src/OcelotTutorials/Basic/ApiGateway/Program.cs
+++ microservices/src/OcelotTutorials/Basic/ApiGateway/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.Identity.Web;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
+builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.Configuration);
+// Ocelot configuration
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,9 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseOcelot().Wait() ;
 app.UseAuthorization();
-
 app.MapControllers();
 
+app.UseOcelot().Wait();
+app.UseAuthentication();
 app.Run();
