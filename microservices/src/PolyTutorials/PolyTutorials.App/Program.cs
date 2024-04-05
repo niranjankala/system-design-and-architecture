256be7bc3a9f9a9f4c46a81d566052236070d27e
Niranjan Singh <niranjan.kala@gmail.com>
2024-04-05 10:33:32 +0530

    Created initial project to start explorering Poly for microservices

---
+++ microservices/src/PolyTutorials/PolyTutorials.App/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace PolyTutorials.App
+{
+    internal class Program
+    {
+        static int counter = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        static void SimulateOperation()
+        {
+
+            counter++;
+            Console.WriteLine("Retried");
+            //Simulate an operation that may throw an exception
+            if (counter == 3)
+            {
+                counter = 0;
+                Console.WriteLine("No Error");
+            }
+            else
+            {
+                throw new Exception("Simulation failure");
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
